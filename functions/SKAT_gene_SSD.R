c312a3630ae42873f627bfa8db9dbdc22a58ec9e
Flumenlucidum <49855759+Flumenlucidum@users.noreply.github.com>
2020-10-27 18:10:53 +0900

    Update SKAT_gene_SSD.R

--- functions/SKAT_gene_SSD.R
+++ functions/SKAT_gene_SSD.R
@@ -36,17 +36,22 @@ SKAT_gene_SSD<-function(anno,bfile,resultfilename,cov=NULL,method='SKAT',weights
   } 
   
   #change the name of the snps ID to prevent duplication
-  command<-paste0(' --bfile ',bfile, ' --set-all-var-ids @:#:\\$r:\\$a --new-id-max-allele-len 300 --make-bed --out changed_',bfile)
+  command2<-paste0(' --bfile ',bfile, ' --set-all-var-ids @:#:\\$r:\\$a --new-id-max-allele-len 300 --make-bed --out changed_',bfile)
   if(plinkver==1){
     if(sum(mapply(is.element, 'plink', list.files('./', 'plink', recursive=TRUE, full.names=FALSE)[1])) < 1){
     tryCatch(stop("The plink file is not in the current directory")})
   }
+    bim<-read.table(paste0(bfile,'.bim'))
+    bim$V2<-paste0(as.character(bim$V1),':',as.character(bim$V4),':',as.character(bim$V6),':',as.character(bim$V5))
+    write.table(bim,file=paste0('changed_',bfile,'.bim'),quote=F,row.names = F,col.names = F)
+    command<-paste0(' --bed ',bfile, '.bed --bim changed_',bfile,'.bim --fam ',bfile, '.fam --make-bed --out changed_',bfile)
+
     system2('./plink',command,wait=T)
   }else{
     if(sum(mapply(is.element, 'plink2', list.files('./', 'plink2', recursive=TRUE, full.names=FALSE)[1])) < 1){
     tryCatch(stop("The plink2 file is not in the current directory")})
   }
-    system2('./plink2',command,wait=T)
+    system2('./plink2',command2,wait=T)
   }
   anno<-fread(anno,fill=T)
   genelist<-unique(anno$Gene.refGene)
