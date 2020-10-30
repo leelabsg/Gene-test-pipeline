338084cf8e7ebe2141ff9a418f6e9ea3aea58e5f
leelabsg <73377376+leelabsg@users.noreply.github.com>
2020-10-30 11:18:00 +0900

    Update SKAT_gene_SSD_All_functions.R

--- functions/SKAT_gene_SSD_All_functions.R
+++ functions/SKAT_gene_SSD_All_functions.R
@@ -524,7 +524,7 @@ SKAT_gene_SSD_specific<-function(anno,bfile,gene=c(),resultfilename,cov=NULL,met
 
     system2('./plink',command,wait=T)
   }else{
-    if(sum(mapply(is.element, 'plink2', list.files('./', 'plink2', recursive=TRUE, full.names=FALSE)[1])) < 1){
+    if(!file.exists('./plink2')){
     tryCatch(stop("The plink2 file is not in the current directory")})
   }
     system2('./plink2',command2,wait=T)
