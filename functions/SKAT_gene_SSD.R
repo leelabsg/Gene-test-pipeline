4f5333b4ae59e865d640207ac8bba0700067bf59
leelabsg <73377376+leelabsg@users.noreply.github.com>
2020-11-23 14:18:32 +0900

    Update SKAT_gene_SSD.R

--- functions/SKAT_gene_SSD.R
+++ functions/SKAT_gene_SSD.R
@@ -1,5 +1,5 @@
 ##SKAT_gene_SSD function
-SKAT_gene_SSD<-function(anno,bfile,resultfilename,cov=NULL,method='SKAT',weights.beta=c(1,25),weights=NULL,Is.binary,genefunc=c(),exonicfunc=c(),number=1000,leaveSSD=F,plinkver=2){
+SKAT_gene_SSD<-function(anno,bfile,resultfilename,cov=NULL,method='SKATO',weights.beta=c(1,25),weights=NULL,Is.binary,genefunc=c(),exonicfunc=c(),number=1000,leaveSSD=F,plinkver=2){
   #making fam_cov file 
   #object for continuous phenotype
   if(!Is.binary){
